ae02c11e3de0aed6dd849775c2595456bbc2385b
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-25 11:54:05 -0400

    Phase 3 nested arrays iteration lab part 2

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,17 @@
+def find_min_integer_array(array)
+  sortedArray = array.sort
+  sortedArray[0]
+end
+
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  retArray = []
+  rowIndex = 0
+  while rowIndex < src.count do
+    columnIndex = 0
+    retArray << find_min_integer_array(src[rowIndex])
+    rowIndex += 1
+  end
+  retArray
 end
